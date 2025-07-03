692b6bb34ffb530221c11d12e8f70709229a9b43
Angel Luis Flores Diaz <angel.flores280303@gmail.com>
2025-07-03 16:02:33 -0400

    culminaciono de la pagina de Dashboard a nivel de forntend

--- Pages/dashboard.py
+++ Pages/dashboard.py
@@ -28,17 +28,95 @@ def Dashboard(page: ft.Page):
     page.title = "MKQueue - Dashboard"
     page.padding = ft.padding.all(0)
     
+    #* Controles de la pagina
 
+    #? Contenedor para las queue trees activas
+    Value_Active_QueueTree = ft.Text(style=styles.Values_Style)
+    Active_QueueTree = styles.ContainerStyle1(
+        content=ft.Column(
+            [
+                ft.Image(src="Recursos/Iconos/QueueTreeIcon.png", width=40, height=80, fit=ft.ImageFit.CONTAIN),
+                Value_Active_QueueTree,
+                ft.Text("Active Queue Tree", style=styles.ContainerLabel_style)
+            ]
+            )
+    )
+
+    #? Contenedor para las queue types activas
+    Value_Active_QueueType = ft.Text(style=styles.Values_Style)
+    Active_QueueType = styles.ContainerStyle1(
+        content=ft.Column(
+            [
+                ft.Image(src="Recursos/Iconos/QueueTypeIcon.png", width=40, height=80, fit=ft.ImageFit.CONTAIN),
+                Value_Active_QueueType,
+                ft.Text("Active Queue Type", style=styles.ContainerLabel_style)
+            ]
+            )
+    )
+
+    #? Contenedor para los router activos
+    Value_Active_Router = ft.Text(style=styles.Values_Style)
+    Active_Router = styles.ContainerStyle1(
+        content=ft.Column(
+            [
+                ft.Image(src="Recursos/Iconos/RouterIcon.png", width=40, height=80, fit=ft.ImageFit.CONTAIN),
+                Value_Active_Router,
+                ft.Text("Active Router", style=styles.ContainerLabel_style)
+            ]
+            )
+    )
+
+    #? Conetendor para la Grafica de tendencia en WAN
+    Traffic_WAN = styles.ContainerStyles2(
+        content=ft.Column(
+            [
+                ft.Divider(height=10, color= ft.Colors.TRANSPARENT),
+                ft.Text("Traffic WAN", style=styles.Page_Subtitle,),
+            ],
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER
+        )
+    )
+
+    #* Limpieza de la Pagina y adición de controles
     page.clean()
     page.add(
         ft.Row(
             controls=[
-                Navigation_Bar.Navigation_Bar,
-                ft.Text("Dashboard"),
+                ft.Column(
+                    controls=[
+                        Navigation_Bar.Navigation_Bar,
+                    ],
+                ),
+                ft.Column(
+                    controls=[
+                        ft.VerticalDivider(width=25)
+                    ]
+                ),
+                ft.Column(
+                    controls=[
+                        ft.Row(height= 10),
+                        ft.Text("Dashboard", style=styles.Page_Title,),
+                        ft.Row(height= 20),
+                        ft.Row(
+                            [
+                                Active_QueueTree,
+                                Active_QueueType,
+                                Active_Router
+                            ],
+                            spacing= 30,
+                        ),
+                        ft.Row(height= 20),
+                        ft.Row(
+                            [
+                                Traffic_WAN,
+                            ]
+                        ),
+                    ]
+                ),
             ],
             alignment=ft.MainAxisAlignment.START,
             vertical_alignment=ft.CrossAxisAlignment.START,
-        )
+        ),    
     )
 
 #! Ejecutar la aplicacion siempre que se ejecute desde este archivo
