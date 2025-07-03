b847637f1b5c176776ffc654342fc979198b5e11
Angel Luis Flores Diaz <angel.flores280303@gmail.com>
2025-07-03 17:00:08 -0400

    Culminacion de Frontend en pagina QueueTree

---
+++ Pages/QueueTree.py
@@ -0,0 +1,130 @@
+# MKQueue/Pages/QueueTree.py
+
+#! Importaciones
+import sys
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+app_root = os.path.dirname(current_dir)
+sys.path.insert(0, app_root)
+
+import flet as ft
+from Objects import Navigation_Bar
+from Styles import styles
+
+#! Pagina del Arbol de Colas
+def QueueTree(page: ft.Page):
+    #* Estilos de la pagina
+    page.theme_mode = ft.ThemeMode.LIGHT
+    page.bgcolor = '#FFFFFF'
+    page.window.width = ft.Window.width
+    page.window.height = 900
+    page.window.resizable = False
+    page.window.maximizable = False
+    page.fonts = {
+        "Inter": "Recursos/Fuentes/Inter.ttf",
+    }
+    page.theme = ft.Theme(font_family="Inter")
+    page.title = "MKQueue - Queue Tree"
+    page.padding = ft.padding.all(0)
+
+    #* Controles de la pagina
+
+    #? Boton para cargar un archivo
+    Load_File_Button = ft.ElevatedButton(
+        style=styles.Primary_Button,
+        text= "Load File",
+        width= 160,
+        height= 60,
+        icon= ft.Icons.FOLDER,
+        icon_color= ft.Colors.WHITE,
+    )
+
+    #? Boton para Sincronizar con el Router
+    Sync_Router_Button = ft.ElevatedButton(
+        style=styles.Primary_Button,
+        text= "Sync To Router",
+        width= 160,
+        height= 60,
+        icon= ft.Icons.SYNC_ALT,
+        icon_color= ft.Colors.WHITE,
+    )
+
+    #? Boton para sincronizar desde el router
+    Sync_From_Router_Button = ft.ElevatedButton(
+        style=styles.Primary_Button,
+        text= "Sync From Router",
+        width= 160,
+        height= 60,
+        icon= ft.Icons.SYNC,
+        icon_color= ft.Colors.WHITE,
+    )
+
+    #? Boton para Agregar una nueva cola
+    Add_Queue_Button = ft.ElevatedButton(
+        style=styles.Primary_Button,
+        text= "Add New Queue Tree",
+        width= 160,
+        height= 60,
+        icon= ft.Icons.ADD,
+        icon_color= ft.Colors.WHITE,
+    )
+
+    #? Contenedor para Lista de Colas
+    Queue_List_Container = ft.Container(
+        width= 900,
+        height= 700,
+        bgcolor= "#FBFAF9",
+        border_radius= ft.border_radius.all(10),
+        border= ft.border.all(2, "#EDEDED"),
+        padding=ft.padding.all(10),
+        content=ft.Column(
+            [
+                ft.Divider(height=10, color= ft.Colors.TRANSPARENT),
+                ft.Text("Configured Queue Trees", style=styles.Page_Subtitle,),
+            ],
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER
+        )
+    )
+
+    #* Limpieza de la Pagina y adición de controles
+    page.clean()
+    page.add(
+        ft.Row(
+            controls=[
+                ft.Column(
+                    controls=[
+                        Navigation_Bar.Navigation_Bar,
+                    ],
+                ),
+                ft.Column(
+                    controls=[
+                        ft.VerticalDivider(width=25)
+                    ]
+                ),
+                ft.Column(
+                    controls=[
+                        ft.Row(height=10),
+                        ft.Row(
+                            controls=[
+                                ft.Text("Queue Tree Management",style=styles.Page_Title, width= 200),
+                                ft.VerticalDivider(width=5, color= ft.Colors.TRANSPARENT),
+                                Load_File_Button,
+                                Sync_Router_Button,
+                                Sync_From_Router_Button,
+                                Add_Queue_Button,
+                            ],
+                        ),
+                        ft.Row(height=20),
+                        Queue_List_Container
+                    ]
+                )
+            ],
+            alignment=ft.MainAxisAlignment.START,
+            vertical_alignment=ft.CrossAxisAlignment.START
+        )
+    )
+
+#! Ejecutar la aplicacion siempre que se ejecute desde este archivo
+if __name__ == "__main__":
+    ft.app(QueueTree)
