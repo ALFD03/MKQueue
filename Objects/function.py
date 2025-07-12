9d8e59ea0a3d73aef4c5f504e2a7b01dd3157fa4
Angel Luis Flores Diaz <angelluisflores03@gmail.com>
2025-07-12 10:37:31 -0400

    Se creo la funcion para limpirar la pagina se creo la funion para insertar un nuevo usuario en la base de datos

--- Objects/function.py
+++ Objects/function.py
@@ -2,6 +2,7 @@
 
 #! Importaciones
 import flet as ft
+from Database.querys import Connect_db
 
 #! Funciones de Navegacion
 
@@ -51,3 +52,32 @@ def navigate_to_add_new_router(page):
 def navigate_to_add_new_queue_tree(page):
     from Pages.Sub_Pages.Add_Queue_tree import Add_Queue_tree
     Add_Queue_tree(page)
+
+#! Funciones para eventos
+
+#? Funcion de Agregar user
+def AddNewUser (Username, Email, Name, Last_name, Password, Confirm_Password, Privileges):
+    conn = Connect_db()
+    psql = conn.cursor()
+    psql.execute(
+        "INSERT INTO users (username, email, name, last_name, password, confirm_password, privileges) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+        (
+            Username,
+            Email,
+            Name,
+            Last_name,
+            Password,
+            Confirm_Password,
+            Privileges
+        )
+    )
+    conn.commit()
+    psql.close()
+    conn.close()
+
+#? Funcion de Limpieza de controles
+def clear_controls(page, function_page):
+    function_page(page)
+
+
+            
